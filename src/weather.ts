61bcc9554ff3731ba7e79bf992acb070d7507d2f
Moussa <mossa.tnm31@gmail.com>
2022-06-17 15:11:19 +0200

    fixed identation errors

--- src/weather.ts
+++ src/weather.ts
@@ -8,35 +8,35 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 // OpenWeatherMap API gives temp. in kelvin
 function kelvinToCelsius(kelvin: number) {
-    let fahrenheit = ((kelvin - 273.15) * 1.8) + 32;
-    let celsius = (fahrenheit - 32) / 1.8;
-    return Math.floor(celsius);
+	const fahrenheit = ((kelvin - 273.15) * 1.8) + 32;
+	const celsius = (fahrenheit - 32) / 1.8;
+	return Math.floor(celsius);
 }
 
 const getWeather = async(location: string) => {
-    var key = process.env.API_KEY;
-    try{
-        const listRsp = await fetch(
-            'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + key
-        );
-        const jsonFile: any = await listRsp.json();
-        const dataToDisplay: WeatherInterface[] = [];
-        dataToDisplay.push({
-            //data to push
-            main: jsonFile.main.name,
-            temp: kelvinToCelsius(jsonFile.main.temp),
-            feelsLike: kelvinToCelsius(jsonFile.main.feels_like),
-            tempMin: kelvinToCelsius(jsonFile.main.temp_min),
-            tempMax: kelvinToCelsius(jsonFile.main.temp_max),
-            humidity: jsonFile.main.humidity,
-            country: jsonFile.sys.country,
-            name: jsonFile.name,
-            weatherDescription: jsonFile.weather[0].description,
-        });
-        return dataToDisplay;
-    } catch (error) {
-        throw new API_ERROR('Unvalid Request');
-    }
-}
+	const key = process.env.API_KEY;
+	try{
+		const listRsp = await fetch(
+			'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + key
+		);
+		const jsonFile: any = await listRsp.json();
+		const dataToDisplay: WeatherInterface[] = [];
+		dataToDisplay.push({
+			//data to push
+			main: jsonFile.main.name,
+			temp: kelvinToCelsius(jsonFile.main.temp),
+			feelsLike: kelvinToCelsius(jsonFile.main.feels_like),
+			tempMin: kelvinToCelsius(jsonFile.main.temp_min),
+			tempMax: kelvinToCelsius(jsonFile.main.temp_max),
+			humidity: jsonFile.main.humidity,
+			country: jsonFile.sys.country,
+			name: jsonFile.name,
+			weatherDescription: jsonFile.weather[0].description,
+		});
+		return dataToDisplay;
+	} catch (error) {
+		throw new API_ERROR('Unvalid Request');
+	}
+};
 
-export {getWeather}
+export {getWeather};
